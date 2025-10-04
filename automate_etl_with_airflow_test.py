dd3b12ae85a3cebae3e96992af0a79c0269da429
Your Name <you@example.com>
2025-10-04 15:35:12 +0900

    .

--- automate_etl_with_airflow_test.py
+++ automate_etl_with_airflow_test.py
@@ -1,6 +1,6 @@
 import time
 from datetime import datetime
-from airflow.models.dag import DAG
+# from airflow.models.dag import DAG
 from airflow.decorators import task
 from airflow.utils.task_group import TaskGroup
 # from airflow.providers.microsoft.mssql.hooks.mssql import MsSqlHook
@@ -8,30 +8,29 @@ from airflow.hooks.base_hook import BaseHook
 import pandas as pd
 from sqlalchemy import create_engine
 
-#extract tasks
-@task()
-def get_src_tables():
-    # hook = MsSqlHook(mssql_conn_id="sqlserver")
-    # sql = """ select  t.name as table_name  
-    #  from sys.tables t where t.name in ('DimProduct','DimProductSubcategory','DimProductCategory') """
-    # df = hook.get_pandas_df(sql)
-    # print(df)
-    # tbl_dict = df.to_dict('dict')
-    # return tbl_dict
 
-    ## database: test_db,  User: test_user, pwd: test,  table: tbl_test
-    conn = BaseHook.get_connection('postgres_test')
-    engine = create_engine(f'postgresql://{conn.login}:{conn.password}@{conn.host}:{conn.port}/{conn.schema}')
-    pc = pd.read_sql_query('SELECT * FROM tbl_test', engine)
-    print(pc)
-    return pc
 
-# [START how_to_task_group]
-with DAG(dag_id="product_etl_dag",schedule_interval="0 9 * * *", start_date=datetime(2022, 3, 5),catchup=False,  tags=["product_model"]) as dag:
+from datetime import datetime
 
-    with TaskGroup("extract_dimProudcts_load", tooltip="Extract and load source data") as extract_load_src:
-        src_product_tbls = get_src_tables()
-        #define order
-        src_product_tbls
+from airflow import DAG
+# from airflow.decorators import task
+from airflow.operators.bash import BashOperator
 
-    extract_load_src
+# A DAG represents a workflow, a collection of tasks
+with DAG(dag_id="automate_etl_with_airflow_test", start_date=datetime(2022, 1, 1), schedule="0 0 * * *") as dag:
+
+    # Tasks are represented as operators
+    hello = BashOperator(task_id="hello", bash_command="echo 'hello'")  # pyright: ignore[reportUndefinedVariable]
+
+    @task()
+    def airflow():
+        # print("airflow")
+         ## database: test_db,  User: test_user, pwd: test,  table: tbl_test
+        conn = BaseHook.get_connection('postgres_test')
+        engine = create_engine(f'postgresql://{conn.login}:{conn.password}@{conn.host}:{conn.port}/{conn.schema}')
+        pc = pd.read_sql_query('SELECT * FROM tbl_test', engine)
+        print(pc)
+        return pc
+
+    # Set dependencies between tasks
+    hello >> airflow()
